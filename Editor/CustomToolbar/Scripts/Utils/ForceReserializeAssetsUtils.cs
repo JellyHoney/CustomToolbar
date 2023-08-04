d6a65027e20a710bc1d289f2db23649cc0739ccc
chenjh2 <chenjh2@g-bits.com>
2023-08-04 15:18:22 +0800

    重新序列化支持递归序列化文件夹

--- Editor/CustomToolbar/Scripts/Utils/ForceReserializeAssetsUtils.cs
+++ Editor/CustomToolbar/Scripts/Utils/ForceReserializeAssetsUtils.cs
@@ -1,26 +1,57 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 
-
-namespace UnityToolbarExtender {
-    public static class ForceReserializeAssetsUtils {
-        public static void ForceReserializeAllAssets() {
-            if (!EditorUtility.DisplayDialog("Attention", "Do you want to force reserialize all assets? This can be time heavy operation and result in massive list of changes.", "Ok", "Cancel")) {
+namespace UnityToolbarExtender
+{
+    public static class ForceReserializeAssetsUtils
+    {
+        public static void ForceReserializeAllAssets()
+        {
+            if (!EditorUtility.DisplayDialog("Attention", "Do you want to force reserialize all assets? This can be time heavy operation and result in massive list of changes.", "Ok", "Cancel"))
+            {
                 return;
             }
 
             AssetDatabase.ForceReserializeAssets();
         }
 
-        public static void ForceReserializeSelectedAssets() {
-            var assetGUIDs = Selection.assetGUIDs;
-            if (assetGUIDs.Length == 0) {
+        public static void ForceReserializeSelectedAssets()
+        {
+            // 获取当前选中的资源对象数组
+            string[] selectedAssetGUIDs = Selection.assetGUIDs;
+            if (selectedAssetGUIDs.Length == 0)
+            {
                 EditorUtility.DisplayDialog("Attention", "No assets are selected.", "Ok");
                 return;
             }
 
-            var assetPaths = Array.ConvertAll<string, string>(assetGUIDs, AssetDatabase.GUIDToAssetPath);
-            AssetDatabase.ForceReserializeAssets(assetPaths);
+            var assetPathSet = new HashSet<string>();
+            // 遍历选中的资源对象
+            foreach (var selectedAssetGUID in selectedAssetGUIDs)
+            {
+                var assetPath = AssetDatabase.GUIDToAssetPath(selectedAssetGUID);
+                var selectedObject = AssetDatabase.LoadAssetAtPath<Object>(assetPath);
+
+                // 检查是否是文件夹类型资源
+                if (selectedObject is DefaultAsset folder)
+                {
+                    // 获取文件夹下的所有资源路径
+                    string[] assetGUIDs = AssetDatabase.FindAssets("", new[] { AssetDatabase.GetAssetPath(folder) });
+
+                    // 遍历文件夹下的所有资源
+                    foreach (string assetGUID in assetGUIDs)
+                    {
+                        assetPathSet.Add(AssetDatabase.GUIDToAssetPath(assetGUID));
+                    }
+                }
+                else
+                {
+                    assetPathSet.Add(AssetDatabase.GetAssetPath(selectedObject));
+                }
+            }
+
+            AssetDatabase.ForceReserializeAssets(assetPathSet, ForceReserializeAssetsOptions.ReserializeAssetsAndMetadata);
         }
     }
 }
